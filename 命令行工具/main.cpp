4860de5a59cd10d34d9fc05646cc29bde9165e2e
COLID <16058094+colid@user.noreply.gitee.com>
2025-08-21 13:03:32 +0800

    A

--- 命令行工具/main.cpp
+++ 命令行工具/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     string command = argv[1];
     // 输出每个命令行参数
     if(command == "init"){
-        int result = std::system("git clone https;//bgithub.xyz/fanghuangxu/62.git && cd 62 && del /f /s /q .git && rm -rf .git && git init");
+        int result = std::system("git clone https://gitee.com/colid/62cct.git && cd 62 && del /f /s /q .git && rm -rf .git && git init && git remote add origin https://gitee.com/colid/62cct.git && git add . && git commit -m 'init' && git config --global user.name \\\"COLID\\\" && git config --global user.email \\\"16058094+colid@user.noreply.gitee.com\\\" && git push -u origin \\\"master\\\" --force && git config --global user.name "" && git config --global user.email \\\"\\\"");
         if (result == 0) {
             cout << "初始化成功！" << "\n";
         } else {
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
         }
     }else if(command == "push"){
         cout << "正在推送仓库..." << "\n";
-        int result = std::system("git push");
+        int result = std::system("git push --force");
         if (result == 0) {
             cout << "推送成功！" << "\n";
         } else {
