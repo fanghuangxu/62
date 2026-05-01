bbfa5a4929331f1382b9b363831c2955e8a83421
COLID <16058094+colid@user.noreply.gitee.com>
2026-05-01 21:24:50 +0800

    紧急修复 bug

--- scripts/script4.py
+++ scripts/script4.py
@@ -7,13 +7,13 @@ template = """
     <title>{name}</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="..\styles\students.css">
-    <link rel="icon" href="https://bgithub.xyz/fanghuangxu/62/raw/refs/heads/main/icons/icon/fe43525e778ed14a76bd691e3cd8c807.png" type="image/png">
+    <link rel="icon" href="https://gh-proxy.org/https://github.com/fanghuangxu/62/raw/refs/heads/main/icons/icon/fe43525e778ed14a76bd691e3cd8c807.png" type="image/png">
 </head>
 
 <body>
     <div class="student-profile" style="width: 500px; height: 600px;">
     <h1>{name}</h1>
-    <img src="https://bgithub.xyz/fanghuangxu/62/raw/refs/heads/main/icons/students/{number}.jpg" alt="head image" style="width: 200px; height: 200px; object-fit: cover;">
+    <img src="https://gh-proxy.org/https://github.com/fanghuangxu/62/raw/refs/heads/main/icons/students/{number}.jpg" alt="head image" style="width: 200px; height: 200px; object-fit: cover;">
     <p>名字: {name}</p>
     <p>座号: {number}</p>
     <div class="contact">
